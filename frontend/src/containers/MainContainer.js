80f220ca2d07e39e067519b6cb3ac29504f4c863
Ace Jordan <acelumaad@live.com>
2019-04-14 20:06:33 +0800

    added material ui theme

--- frontend/src/containers/MainContainer.js
+++ frontend/src/containers/MainContainer.js
@@ -24,13 +24,13 @@ function ButtonAppBar(props) {
   const { classes } = props;
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar color="primary">
         <Toolbar>
           <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" color="inherit" className={classes.grow}>
-            News
+            CLINation
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,23 @@ import { Provider } from 'react-redux'
 import configureStore from 'store'
 import Routes from 'routes'
 
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 import registerServiceWorker from 'utils/registerServiceWorker'
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#2196f3'
+    }
+  }
+})
+
 render(
-  <Provider store={configureStore()}>
-    <Routes />
-  </Provider>,
+  <MuiThemeProvider theme={theme}>
+    <Provider store={configureStore()}>
+      <Routes />
+    </Provider>
+  </MuiThemeProvider>,
   document.getElementById('root')
 )
 
