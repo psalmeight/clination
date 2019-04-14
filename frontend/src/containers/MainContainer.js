2fb710eed4ccbaa6a0e8537bdc1b794f34c6f79d
Ace Jordan <acelumaad@live.com>
2019-04-14 21:22:25 +0800

    login page

--- frontend/src/containers/MainContainer.js
+++ frontend/src/containers/MainContainer.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import { withStyles } from '@material-ui/core/styles';
-import MenuIcon from '@material-ui/icons/Menu';
-
+import MenuIcon from '@material-ui/icons/Menu'
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles'
 import {
   Modal, AppBar, Toolbar, Typography, Button, IconButton, Grid, Paper, TextField
 } from '@material-ui/core'
 
 const styles = {
-  root: {
-    flexGrow: 1,
-  },
-  grow: {
-    flexGrow: 1,
-  }
-};
+    root: {
+      flexGrow: 1,
+    },
+    grow: {
+      flexGrow: 1,
+    }
+}
 
 class MainContainer extends React.Component {
-    state = {
-        showRegistration: false
-    }
-
     constructor(props){
         super(props)
     }
 
-    showRegistration = (val) => {
+    state = {
+        showRegistration: false
+    }
+
+    showRegistration = val => {
         this.setState({ showRegistration: val })
     }
 
@@ -32,7 +32,7 @@ class MainContainer extends React.Component {
     
         return (
             <div>
-                <AppBar color="primary" positionStatic>
+                <AppBar color="primary" position="static">
                     <Toolbar>
                         <IconButton color="inherit" aria-label="Menu">
                             <MenuIcon />
@@ -45,8 +45,8 @@ class MainContainer extends React.Component {
                     </Toolbar>
                 </AppBar>
       
-                <Grid container style={{ marginTop: 80 }} spacing={8}>
-                    <Grid item md={6} xs={12}>
+                <Grid container style={{ marginTop: 150 }} spacing={8} justify="center" alignContent="center">
+                    <Grid item>
                         <form noValidate autoComplete="off">
                             <TextField
                                 id="username"
@@ -55,6 +55,9 @@ class MainContainer extends React.Component {
                                 fullWidth
                                 margin="dense"
                                 variant="outlined"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
                             />
       
                             <TextField
@@ -64,60 +67,105 @@ class MainContainer extends React.Component {
                                 fullWidth
                                 margin="dense"
                                 variant="outlined"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
                             />
+
+                            <Button variant="contained" fullWidth color="primary" onClick={() => this.showRegistration(false)} style={{ marginTop: 10 }}>
+                                Login
+                            </Button>
                         </form>
-                    </Grid>
-                    <Grid item md={6} xs={12}>
-            
                     </Grid>
                 </Grid>
             
-                <Modal open={this.state.showRegistration}>
-                    <form noValidate autoComplete="off">
-                        <TextField
-                            id="fullname"
-                            label="Fullname"
-                            placeholder="Enter Fullname"
-                            fullWidth
-                            margin="dense"
-                            variant="outlined"
-                        />
-      
-                        <TextField
-                            id="username"
-                            label="Username"
-                            placeholder="Enter Username"
-                            fullWidth
-                            margin="dense"
-                            variant="outlined"
-                        />
-      
-                        <TextField
-                            id="email"
-                            label="Email"
-                            placeholder="Enter Email"
-                            fullWidth
-                            margin="dense"
-                            variant="outlined"
-                        />
-      
-                        <TextField
-                            id="contact"
-                            label="Contact Number"
-                            placeholder="Enter Contact Number"
-                            fullWidth
-                            margin="dense"
-                            variant="outlined"
-                        />
-                    </form>
+                <Modal open={this.state.showRegistration} style={{ marginTop: '10%' }}>
+                    <Grid 
+                        container
+                        direction="row"
+                        justify="center"
+                        alignItems="center">
+                        <Grid item>
+                            <Paper elevation={2} style={{ padding: 20 }}>
+                                <Typography variant="h6" style={{ marginBottom: 20 }}>
+                                    Clinic Registration Fields
+                                </Typography>
+                                <form noValidate autoComplete="off">
+                                    <TextField
+                                        id="fullname"
+                                        label="Fullname"
+                                        placeholder="Enter Fullname"
+                                        fullWidth
+                                        margin="dense"
+                                        variant="outlined"
+                                        InputLabelProps={{
+                                            shrink: true,
+                                        }}
+                                    />
+                
+                                    <TextField
+                                        id="username"
+                                        label="Username"
+                                        placeholder="Enter Username"
+                                        fullWidth
+                                        margin="dense"
+                                        variant="outlined"
+                                        InputLabelProps={{
+                                            shrink: true,
+                                        }}
+                                    />
+                
+                                    <TextField
+                                        id="email"
+                                        label="Email"
+                                        placeholder="Enter Email"
+                                        fullWidth
+                                        margin="dense"
+                                        variant="outlined"
+                                        InputLabelProps={{
+                                            shrink: true,
+                                        }}
+                                    />
+                
+                                    <TextField
+                                        id="contact"
+                                        label="Contact Number"
+                                        placeholder="Enter Contact Number"
+                                        fullWidth
+                                        margin="dense"
+                                        variant="outlined"
+                                        InputLabelProps={{
+                                            shrink: true,
+                                        }}
+                                    />
+                                    
+                                    <Grid direction="row" container>
+                                        <Grid item md={6}>
+                                            <Button variant="contained" color="primary" onClick={() => this.showRegistration(false)} style={{ marginTop: 20 }}>
+                                                Register Clinic
+                                            </Button>
+                                        </Grid>
+                                        <Grid item md={6} alignContent="flex-end">
+                                            <Button variant="contained" color="primary" onClick={() => this.showRegistration(false)} style={{ marginTop: 20 }}>
+                                                Cancel
+                                            </Button>
+                                        </Grid>
+                                    </Grid>
 
-                    <Button variant="contained" color="primary" onClick={() => this.showRegistration(false)}>
-                        Register Clinic
-                    </Button>
+                                </form>
+                            </Paper>
+                        </Grid>
+                    </Grid>
                 </Modal>
             </div>
         )
     }
 }
 
-export default withStyles(styles)(MainContainer);
+MainContainer.propTypes = {
+    classes: PropTypes.object.isRequired,
+}
+
+const WrappedMainContainer = withStyles(styles)(MainContainer)
+
+export default WrappedMainContainer
