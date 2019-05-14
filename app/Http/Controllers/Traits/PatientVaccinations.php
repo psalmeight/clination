3d9f55e3e35fc191dd4b29f3c26c56f9abb23bfd
Ace Jordan <acelumaad@live.com>
2019-05-14 13:23:53 +0800

    patient vaccination trait

---
+++ app/Http/Controllers/Traits/PatientVaccinations.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\Traits;
+
+use App\Models\PatientVaccination;
+use App\Models\Patient;
+
+use App\User;
+use Auth;
+use Illuminate\Support\Facades\Log;
+
+trait PatientVaccinationTrait
+{
+    public function func_savePatientVaccination($request)
+    {
+        $obj = new PatientVaccination;
+
+        $patient = Patient::find($request->patient);
+
+        $obj->patient_id = $patient->getKey();
+
+        $obj->visit_datetime = $request->visit_datetime;
+        $obj->vaccination_date = $request->vaccination_date;
+        $obj->vaccination_notes = $request->vaccination_notes;
+
+        $obj->save();
+
+        return $obj->id;
+    }
+
+    public function func_getPatientVaccinations()
+    {
+        $objectList = PatientVaccination::orderBy('created_at')->get();
+        return $objectList;
+    }
+
+    public function func_getPatientVaccinationsByPatient($patientID)
+    {
+        $objectList = PatientVaccination::where('patient_id', $patientID)->get();
+        return $objectList;
+    }
+
+    public function func_getPatientVaccination($id)
+    {
+        $obj = PatientVaccination::find($id);
+        return $obj;
+    }
+
+    public function func_deletePatientVaccination($objID)
+    {
+        PatientVaccination::destroy($objID);
+        return;
+    }
+}
