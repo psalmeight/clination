ff02613dc28a1ff68fbf72d64a84eb5e16fc1552
Ace Jordan <acelumaad@live.com>
2019-05-17 09:41:26 +0800

    update build

---
+++ frontend/build/precache-manifest.8a9917f690773aa19c2e091f577de0c9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "553401ba89dad973a983",
+    "url": "/static/js/main.a1a315a4.chunk.js"
+  },
+  {
+    "revision": "743a01bb52fcca930e4d",
+    "url": "/static/js/2.dca0e762.chunk.js"
+  },
+  {
+    "revision": "dd7772eccb776a0b05d0d5c08932ecb4",
+    "url": "/index.html"
+  }
+];
