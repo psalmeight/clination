d94ea49309c22df5ed247802694d0d5fe87a445f
Ace Jordan <acelumaad@live.com>
2019-05-22 15:11:03 +0800

    updates

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,11 +25,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('backup:run --only-db')
+        $schedule->exec('pg_dump -U postgres apgclination > /var/www/html/clination/storage/app/backup-temp/temp/db-dumps/apgclination.backup')
         ->everyMinute()
         ->after(function () {
-            $contents = Storage::disk('local')->get('backup-temp/temp/db-dumps/postgresql-apgclination.sql');
-            Storage::disk('s3')->put('clination ' . date('Y-m-d H:i:s'), $contents);
+            $contents = Storage::disk('local')->get('backup-temp/temp/db-dumps/apgclination.backup');
+            Storage::disk('s3')->put('clination_' . date('Y-m-d H:i:s') . '.backup', $contents);
         });;
     }
 
