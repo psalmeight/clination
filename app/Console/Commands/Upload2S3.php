ad961195c79d3150ff81de78f96e49a2fc1ed621
Ace Jordan <acelumaad@live.com>
2019-05-22 17:21:02 +0800

    updates

--- app/Console/Commands/Upload2S3.php
+++ app/Console/Commands/Upload2S3.php
@@ -38,7 +38,7 @@ class Upload2S3 extends Command
      */
     public function handle()
     {
-        $contents = Storage::disk('local')->get('temp/db-dumps/apgclination.backup');
-        Storage::disk('s3')->put('clination_' . date('Y-m-d H:i:s') . '.backup', $contents);
+        $contents = Storage::disk('local')->get('temp/db-dumps/postgresql-apgclination.sql');
+        Storage::disk('s3')->put('clination_' . date('Y-m-d H:i:s') . '.sql', $contents);
     }
 }
